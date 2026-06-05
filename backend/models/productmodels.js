a4d7d4c5550df11eedbc20c683bff155933bb63b
kapil-122414 <kapilkumar122414@gmail.com>
2026-06-05 15:19:31 +0530

    save current changes

--- backend/models/productmodels.js
+++ backend/models/productmodels.js
@@ -55,11 +55,7 @@ const productschema = new Schema(
         colour: String,
         price: Number,
         stock: Number,
-<<<<<<< HEAD
         sku: String,
-=======
-        sku:String,
->>>>>>> 1076ec87c160eea1b27cdf83b6eb41bb2062bc27
       },
     ],
     reviws: [
