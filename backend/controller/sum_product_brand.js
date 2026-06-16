b12fcda7d193b0c43f6fb4d790b8039960a5436b
kapil-122414 <kapilkumar122414@gmail.com>
2026-06-16 18:08:39 +0530

    add the api controller

---
+++ backend/controller/sum_product_brand.js
@@ -0,0 +1,19 @@
+const Brand = require("../models/Brand");
+const Product = require("../models/productmodels");
+
+const totalproduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const brand = await Brand.findById(id);
+    if (!brand) {
+      return res.status(400).json({ message: "brand not found" });
+    }
+    const productcount = await Product.countDocuments({
+      brandId: id,
+    });
+    res.status(200).json({ brand, productcount });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+module.exports = { totalproduct };
