d35da6680109c177b056141b5e453b9ea4da9758
kapil-122414 <kapilkumar122414@gmail.com>
2026-06-18 12:28:51 +0530

    add the auth in brand

--- backend/Routes/BrandRouter.js
+++ backend/Routes/BrandRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const authmiddleware = require("../Middlerware/authmiddleware");
 const brands = require("../models/Brand");
 const Product = require("../models/productmodels");
 const path = require("path");
@@ -9,42 +9,47 @@ const cloudinary = require("../config/cloudinary");
 const { resolve } = require("dns");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-router.post("/brand", uploads.single("Img"), async (req, res) => {
-  try {
-    const data = req.body;
+router.post(
+  "/brand",
+  authmiddleware,
+  uploads.single("Img"),
+  async (req, res) => {
+    try {
+      const data = req.body;
 
-    if (!data.name) {
-      return res.status(400).json({ message: "Brand name is required" });
-    }
-    console.log(req.file);
-    if (!req.file) {
-      return res.status(400).json({
-        message: "Brand image is required",
+      if (!data.name) {
+        return res.status(400).json({ message: "Brand name is required" });
+      }
+      console.log(req.file);
+      if (!req.file) {
+        return res.status(400).json({
+          message: "Brand image is required",
+        });
+      }
+      const finddata = await brands.findOne({ name: data.name });
+      if (finddata) {
+        return res.status(400).json({ message: "Brand already exists" });
+      }
+
+      const savedata = await brands.create({
+        name: data.name,
+        status: data.status,
+        Img: {
+          url: req.file.path,
+          public_id: req.file.filename,
+        },
       });
+
+      res
+        .status(200)
+        .json({ message: "Brand created successfully", data: savedata });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-    const finddata = await brands.findOne({ name: data.name });
-    if (finddata) {
-      return res.status(400).json({ message: "Brand already exists" });
-    }
+  },
+);
 
-    const savedata = await brands.create({
-      name: data.name,
-      status: data.status,
-      Img: {
-        url: req.file.path,
-        public_id: req.file.filename,
-      },
-    });
-
-    res
-      .status(200)
-      .json({ message: "Brand created successfully", data: savedata });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.get("/brand", async (req, res) => {
+router.get("/brand", authmiddleware, async (req, res) => {
   try {
     let filter = {};
     let page = parseInt(req.query.page) || 1;
@@ -93,7 +98,7 @@ router.get("/brand", async (req, res) => {
   }
 });
 
-router.get("/brand/:id", async (req, res) => {
+router.get("/brand/:id", authmiddleware, async (req, res) => {
   try {
     const data = await brands.findById(req.params.id);
 
@@ -107,44 +112,53 @@ router.get("/brand/:id", async (req, res) => {
   }
 });
 
-router.patch("/brand/:id", uploads.single("Img"), async (req, res) => {
-  try {
-    const data = await brands.findById(req.params.id);
+router.patch(
+  "/brand/:id",
+  authmiddleware,
+  uploads.single("Img"),
+  async (req, res) => {
+    try {
+      const data = await brands.findById(req.params.id);
 
-    if (!data) {
-      return res.status(404).json({ message: "Brand not found" });
-    }
-    const updateData = {};
+      if (!data) {
+        return res.status(404).json({ message: "Brand not found" });
+      }
+      const updateData = {};
 
-    if (req.body.name) {
-      updateData.name = req.body.name;
-    }
-
-    if (req.body.status) {
-      updateData.status = req.body.status;
-    }
-
-    if (req.file) {
-      if (data.Img && data.Img.public_id) {
-        await cloudinary.uploader.destroy(data.Img.public_id);
+      if (req.body.name) {
+        updateData.name = req.body.name;
       }
 
-      updateData.Img = {
-        url: req.file.path,
-        public_id: req.file.filename,
-      };
+      if (req.body.status) {
+        updateData.status = req.body.status;
+      }
+
+      if (req.file) {
+        if (data.Img && data.Img.public_id) {
+          await cloudinary.uploader.destroy(data.Img.public_id);
+        }
+
+        updateData.Img = {
+          url: req.file.path,
+          public_id: req.file.filename,
+        };
+      }
+
+      const updated = await brands.findByIdAndUpdate(
+        req.params.id,
+        updateData,
+        {
+          new: true,
+        },
+      );
+      res.status(200).json({ message: "Updated successfully", data: updated });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
+  },
+);
 
-    const updated = await brands.findByIdAndUpdate(req.params.id, updateData, {
-      new: true,
-    });
-    res.status(200).json({ message: "Updated successfully", data: updated });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.delete("/brand/:id", async (req, res) => {
+router.delete("/brand/:id", authmiddleware, async (req, res) => {
   try {
     const data = await brands.findById(req.params.id);
 
@@ -160,7 +174,5 @@ router.delete("/brand/:id", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
-
-
 
 module.exports = router;
